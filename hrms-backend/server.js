dde3da67d6832c1b9d5aa857ffb99d7f8cc34dce
parshuramkumarsitcomp-collab <parshuramkumar.sit.comp@gmail.com>
2026-03-08 08:23:32 +0530

    Update server.js

--- hrms-backend/server.js
+++ hrms-backend/server.js
@@ -6,9 +6,9 @@ const path = require('path');
 
 const app = express()
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 
 // Middleware
 app.use(cors());
